ccd935fef703f8f7469031cd7f3010e977092c14
AlfaGruisPL <alfagruis@gmail.com>
2023-09-16 20:39:40 +0200

    walka

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 exports.PodsumowanieZakladuInformacjeDto = void 0;
 const podsumowanieZakladuInformacje_dto_1 = require("./interface/podsumowanieZakladuInformacje.dto");
-Object.defineProperty(exports, "PodsumowanieZakladuInformacjeDto", { enumerable: true, get: function () { return podsumowanieZakladuInformacje_dto_1.PodsumowanieZakladuInformacjeDto; } });
+Object.defineProperty(exports, "podsumowanieZakladuInformacjeDto", {
+    enumerable: true, get: function () {
+        return podsumowanieZakladuInformacje_dto_1.PodsumowanieZakladuInformacjeDto;
+    }
+});
